a4dd630f5f30a533f9a200515f144bd3d8d6456d
Denis <kostindenis2001@gmail.com>
2026-03-29 21:00:48 +0300

    [DK-6] - Add ability to copy values

---
+++ impl/src/main/java/module-info.java
@@ -0,0 +1,19 @@
+module com.dkostin.avro_viewer.app {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires static lombok;
+    requires org.apache.avro;
+    requires com.fasterxml.jackson.databind;
+    requires org.slf4j;
+    requires java.naming;
+
+    opens com.dkostin.avro_viewer.app.ui to javafx.fxml;
+    opens com.dkostin.avro_viewer.app.config to javafx.fxml;
+
+    exports com.dkostin.avro_viewer.app;
+    opens com.dkostin.avro_viewer.app.ui.component to javafx.fxml;
+    opens com.dkostin.avro_viewer.app.ui.main to javafx.fxml;
+    opens com.dkostin.avro_viewer.app.domain.state to javafx.fxml;
+    opens com.dkostin.avro_viewer.app.domain.model to javafx.fxml;
+}
